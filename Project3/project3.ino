b8a3797dccc6337777eb924c67cd7d3222828bff
iPwnds <florian.braun.2005@gmail.com>
2023-01-06 12:23:24 +0100

    small touches

--- Project3/project3.ino
+++ Project3/project3.ino
@@ -14,38 +14,43 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int sensorVal = analogRead(sensorPin);
+
   Serial.print("sensor value: ");
   Serial.print(sensorVal);
-  
   // convert the ADC reding to voltage
   float voltage = (sensorVal/1024.0) * 5.0;
+
   Serial.print(", Volts: ");
   Serial.print(voltage);
-  
   Serial.print(", degrees C: ");
   // convert the voltage to temperature in degrees
   float temperature = (voltage - .5) * 100;
-  Serial.println(temperature);
   
+  Serial.println(temperature);
+
   if(temperature < baselineTemp){
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
   }
+
   else if(temperature >= baselineTemp+2 && temperature < baselineTemp+4){
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
   }
+
   else if(temperature >= baselineTemp+4 && temperature < baselineTemp+6){
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, LOW);
   }
+
   else if(temperature >= baselineTemp+6){
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
   }
+  
   delay(1);
 }
