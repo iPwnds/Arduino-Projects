332e978f471e39d9c402055fd775e00f6f21e6c3
iPwnds <florian.braun.2005@gmail.com>
2023-01-06 12:23:27 +0100

    small touches

--- Project4/project4.ino
+++ Project4/project4.ino
@@ -1,21 +1,20 @@
-   const int greenLEDPin = 9;
-   const int redLEDPin = 10;
-   const int blueLEDPin = 11;
-   const int redSensorPin = A0;
-   const int greenSensorPin = A1;
-   const int blueSensorPin = A2;
-   
-   int redValue = 0;
-   int greenValue = 0;
-   int blueValue = 0;
-   int redSensorValue = 0;
-   int greenSensorValue = 0;
-   int blueSensorValue = 0;
+const int greenLEDPin = 9;
+const int redLEDPin = 10;
+const int blueLEDPin = 11;
+const int redSensorPin = A0;
+const int greenSensorPin = A1;
+const int blueSensorPin = A2;
+
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
    
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
   pinMode(greenLEDPin, OUTPUT);
   pinMode(redLEDPin, OUTPUT);
   pinMode(blueLEDPin, OUTPUT);
@@ -28,7 +27,6 @@ void loop() {
   greenSensorValue = analogRead(greenSensorPin);
   delay(5);
   blueSensorValue = analogRead(blueSensorPin);
-
   Serial.print("Raw Sensor Values \t red: ");
   Serial.print(redSensorValue);
   Serial.print("\t green: ");
@@ -36,11 +34,9 @@ void loop() {
   Serial.print("\t blue: ");
   Serial.print(blueSensorValue);
   Serial.print("\t\t");
-
   redValue = redSensorValue/4;
   greenValue = greenSensorValue/4;
   blueValue = blueSensorValue/4;
-
   Serial.print("Mapped Sensor Values \t red: ");
   Serial.print(redValue);
   Serial.print("\t green: ");
@@ -48,7 +44,6 @@ void loop() {
   Serial.print("\t blue:");
   Serial.print(blueValue);  
   Serial.print("\n");
-
   analogWrite(redLEDPin, redValue);
   analogWrite(greenLEDPin, greenValue);
   analogWrite(blueLEDPin, blueValue);
