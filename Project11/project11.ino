4432da59cec2946837a65b73bc4157aa605ca42d
iPwnds <florian.braun.2005@gmail.com>
2023-01-06 12:23:18 +0100

    small touches

--- Project11/project11.ino
+++ Project11/project11.ino
@@ -2,6 +2,7 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int switchPin = 6;
+
 int switchState = 0;
 int prevSwitchState = 0;
 int reply;
@@ -9,9 +10,7 @@ int reply;
 void setup() {
     lcd.begin(16, 2);
     pinMode(switchPin, INPUT);
-
     lcd.print("Ask the");
-
     lcd.setCursor(0, 1);
     lcd.print("Crystal Ball!");
 }
@@ -53,5 +52,6 @@ void loop() {
             }
         }
     }
+
     prevSwitchState = switchState;
 }
