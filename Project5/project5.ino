4112737b53d7866b187e35335ea875a4139651b3
iPwnds <florian.braun.2005@gmail.com>
2023-01-06 12:23:29 +0100

    small touches

--- Project5/project5.ino
+++ Project5/project5.ino
@@ -2,12 +2,13 @@
 Servo myServo;
 
 int const potPin = A0;
+
 int potVal;
 int angle;
+
 void setup() {
   // put your setup code here, to run once:
   myServo.attach(9);
-
   Serial.begin(9600);
 }
 
@@ -16,12 +17,10 @@ void loop() {
   potVal = analogRead(potPin);
   Serial.print("potVal: ");
   Serial.print(potVal);
-
   angle = map(potVal, 0, 1023, 0, 179);
   Serial.print(", angle: ");
   Serial.print(angle);
   Serial.print("\n");
-
   myServo.write(angle);
   delay(15);
 }
